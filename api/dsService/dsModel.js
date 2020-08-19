ff38c7fa9f2e52f16d2ad345c4769cdd0302a766
Julia Vu <juliadvu@gmail.com>
2020-08-19 18:26:17 -0500

    Feat: created 2 ds model functions

--- api/dsService/dsModel.js
+++ api/dsService/dsModel.js
@@ -7,12 +7,20 @@ const getPrediction = (x1, x2, x3) => {
   return dsClient.post('/predict', { x1, x2, x3 });
 };
 
-const getViz = (state) => {
-  return dsClient.get(`/viz/${state}`);
+const getViz = (user_id) => {
+  return dsClient.get(`/viz/${user_id}`);
+};
+
+const getMoneyFlow = (user_id) => {
+  return dsClient.get(`/${user_id}/moneyflow`);
+};
+
+const spendingPost = (request) => {
+  return dsClient.post(`/spending`, request);
 };
 
 function add(data) {
   return db('graph_data').update(data);
 }
 
-module.exports = { getPrediction, getViz, add };
+module.exports = { getPrediction, getViz, add, spendingPost, getMoneyFlow };
