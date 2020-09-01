40a6904cb9adc47292a882b63f7f31847183c903
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-09-01 19:43:39 -0400

    Chore: formatting in checkCache

--- api/middleware/checkCache.js
+++ api/middleware/checkCache.js
@@ -4,19 +4,12 @@ const client = redis.createClient(REDIS_PORT);
 // This function checks the cache to see if requested data is in it.
 // If it's in the cache,
 const checkCache = (req, res, next) => {
-  console.log('Checking Cache...');
   const user = req.body.user_ID;
-  console.log('ITS HERE', req.body)
   client.get(user, (err, data) => {
-    if (err) {
-      console.log('error', err);
-      throw err;
-    } 
+    if (err) throw err;
     if (data !== null) {
-      console.log('data', data);
       res.send(data);
     } else {
-      console.log('not in cache', data);
       next();
     }
   });
