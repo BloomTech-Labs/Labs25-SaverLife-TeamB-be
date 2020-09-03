f773b3deaeb19755f0e7ba66739f74de484d59bb
kwmorlock <kmorlockcodes@gmail.com>
2020-09-03 15:11:30 -0400

    chore/formatteddsrouter

--- __tests__/routes/dsRouter404.test.js
+++ __tests__/routes/dsRouter404.test.js
@@ -5,27 +5,27 @@ const server = require('../../api/app.js');
 // fail with any other code, please check paths, and assigned error codes
 
 describe('404 status codes for ds POSTs', () => {
-    it('should return http status code 404', () => {
-      return supertest(server)
-        .post('/data/moneyflow/1')
-        .then((response) => {
-          expect(response.status).toBe(404);
-        });
-    });
-  
-    it('should return http status code 404', () => {
-      return supertest(server)
-        .post('/data/futureBudget/1')
-        .then((response) => {
-          expect(response.status).toBe(404);
-        });
-    });
-  
-    it('should return http status code 404', () => {
-      return supertest(server)
-        .post('/data/spending/1')
-        .then((response) => {
-          expect(response.status).toBe(404);
-        });
-    });
+  it('should return http status code 404', () => {
+    return supertest(server)
+      .post('/data/moneyflow/1')
+      .then((response) => {
+        expect(response.status).toBe(404);
+      });
   });
+
+  it('should return http status code 404', () => {
+    return supertest(server)
+      .post('/data/futureBudget/1')
+      .then((response) => {
+        expect(response.status).toBe(404);
+      });
+  });
+
+  it('should return http status code 404', () => {
+    return supertest(server)
+      .post('/data/spending/1')
+      .then((response) => {
+        expect(response.status).toBe(404);
+      });
+  });
+});
