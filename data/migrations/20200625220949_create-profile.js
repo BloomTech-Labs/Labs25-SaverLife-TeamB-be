80aafcfd9bb308c9a14a2fcd8865c3fffb0864ad
Michail <mbidnyk1@gmail.com>
2020-09-09 17:21:15 -0500

    Chore: Updated profile migration table and added default profile seed data

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -6,8 +6,8 @@ exports.up = (knex) => {
       table.string('email');
       table.string('name');
       table.string('ds_id');
-      table.integer('monthly_savings_goal').defaultTo(null);
-      table.string('user_categories').defaultTo(null);
+      table.integer('monthly_savings_goal');
+      table.string('placeholder');
       table.timestamps(true, true);
     });
 };
