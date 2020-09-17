262f4869fa2c96906e638a2ce0a19195c6d53b10
kwmorlock <kmorlockcodes@gmail.com>
2020-09-17 15:53:54 -0400

    feat/refer-post-testemail

--- api/index/indexRouter.js
+++ api/index/indexRouter.js
@@ -41,4 +41,14 @@ router.get('/refer', (req, res) => {
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+router.post('/contact', (req, res) => {
+  const msg = {
+    to: 'test@example.com',
+    from: 'test@example.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  });
+
 module.exports = router;
